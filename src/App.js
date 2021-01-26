f451be7046234f2b3a89f85991f402da55dc3e4f
Jesse Clark <hello@developerjesse.com>
2021-01-26 18:08:51 +0200

    Add extra sign method.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { getResolver } from 'ethr-did-resolver'
 import Eth from 'ethjs-query'
 import './App.css';
 import { createJWT, verifyJWT } from 'jesse-did-jwt';
+import { fromRpcSig } from 'ethereumjs-util';
 
 
 function App() {
   const [address, setAddress] = useState(null)
   const [credential, setCredential] = useState(null)
+  const [presentation, setPresentation] = useState(null)
   const [message, setMessage] = useState(null)
 
   const providerConfig = {
@@ -20,6 +22,12 @@ function App() {
   }
   const resolver = new Resolver(getResolver(providerConfig))
   const signer = (data) => window.ethereum.request({ method: 'personal_sign', params: [data, address] })
+    .then(fromRpcSig)
+    .then(({ v, r, s }) => ({
+      r: r.toString('hex'),
+      s: s.toString('hex'),
+      recoveryParam: v
+    }))
 
   const handleError = (error) => setMessage(`ERROR: ${error.message}`);
 
@@ -34,17 +42,35 @@ function App() {
 
   const createCredential = () => {
     createJWT(
-      {aud: address, exp: 1957463421, name: 'uPort Developer'},
+      { exp: 1957463421, name: 'uPort Developer'},
       {alg: 'ES256K', issuer: `did:ethr:rsk:testnet:${address}`, signer}
     )
       .then((response) => setCredential(response))
       .catch(handleError)
   }
 
+  const createPresentation = () => {
+    const vpPayload = {
+      vp: {
+        '@context': ['https://www.w3.org/2018/credentials/v1'],
+        type: ['VerifiablePresentation'],
+        verifiableCredential: [credential],
+        nbf: Math.floor(new Date().getTime() / 1000),
+        exp: Math.floor(new Date().getTime() / 1000) + 3600
+      }
+    }
+    createJWT(vpPayload, {alg: 'ES256K', issuer: `did:ethr:rsk:testnet:${address}`, signer})
+      .then((response) => setPresentation(response))
+      .catch(handleError)
+  }
+
   const verifyCredential = (jwt) => {
     setMessage('loading...')
     verifyJWT(jwt, {ethSign: true, resolver: resolver, })
-      .then(res => console.log('res', res))
+      .then(res => {
+        setMessage('SUCCESS!')
+        console.log('res', res)
+      })
       .catch(handleError)
   }
 
@@ -58,8 +84,14 @@ function App() {
       <hr/>
       <div>
         <button disabled={!credential} onClick={() => verifyCredential(credential)}>Verify Credential</button>
-        {message && <span>{message}</span>}
       </div>
+      <hr/>
+      <button disabled={!credential} onClick={createPresentation}>Create Presentation</button>
+      <div style={{wordBreak: 'break-all'}}>{presentation}</div>
+      <hr/>
+      <button disabled={!credential} onClick={() => verifyCredential(presentation)}>Verify Presentation</button>
+      <hr />
+      {message && <span>{message}</span>}
     </div>
   );
 }
